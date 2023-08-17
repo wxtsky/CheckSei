a753c6198536ac761f70bb4fd98fd83dd139245b
wxt669698 <1970550145@qq.com>
2023-08-17 18:45:07 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {Button, Input, Space, Table, Card, Typography} from 'antd';
+import {Button, Input, Space, Table, Card, Typography, Spin, message} from 'antd';
 import {useState} from 'react';
 import axios from 'axios';
 
@@ -7,10 +7,34 @@ const {TextArea} = Input;
 const {Title} = Typography;
 
 function App() {
-    const [data, setdata] = useState([]);
+    const [data, setData] = useState([]);
     const [input, setInput] = useState('');
+    const [loading, setLoading] = useState(false);
+    const refreshRow = async (address, index) => {
+        const url = "https://pacific-1.albatross.sei-internal.com/eligibility?originAddress=" + address.toLowerCase();
+        try {
+            const res = await axios.get(url);
+            const updatedData = [...data];
+            updatedData[index] = {
+                address: address,
+                isEligible: res.data.status,
+                chainId: res.data.data.chainId
+            };
+            setData(updatedData);
+        } catch (e) {
+            const updatedData = [...data];
+            updatedData[index] = {
+                address: address,
+                isEligible: 'error',
+                chainId: '-'
+            };
+            setData(updatedData);
+        }
+    };
+
     const check = async () => {
-        setdata([]);
+        setLoading(true);
+        setData([]);
         const arr = input.split('\n');
         for (let i = 0; i < arr.length; i++) {
             const address = arr[i];
@@ -23,7 +47,7 @@ function App() {
             while (attempts < 3 && !success) {
                 try {
                     const res = await axios.get(url);
-                    setdata(prevData => [...prevData, {
+                    setData(prevData => [...prevData, {
                         address: address,
                         isEligible: res.data.status,
                         chainId: res.data.data.chainId
@@ -34,13 +58,15 @@ function App() {
                 }
             }
             if (!success) {
-                setdata(prevData => [...prevData, {
+                setData(prevData => [...prevData, {
                     address: address,
                     isEligible: 'error',
                     chainId: '-'
                 }]);
             }
         }
+        message.success('查询完成');
+        setLoading(false);
     }
 
     const columns = [
@@ -67,12 +93,23 @@ function App() {
             title: '链ID',
             dataIndex: 'chainId',
             key: 'chainId',
+        },
+        {
+            title: '刷新',
+            key: 'refresh',
+            render: (text, record, index) => (
+                <Button type="primary" onClick={() => refreshRow(record.address, index)}>
+                    刷新
+                </Button>
+            ),
         }
-    ]
+    ];
+
     return (
         <div style={{padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <Title level={2}>Sei 空投资格EVM地址查询</Title>
             <Card style={{width: '80%', marginBottom: '20px'}}>
+                {loading && <div style={{textAlign: 'center'}}><Spin tip="查询中..."/></div>}
                 <TextArea
                     rows={4}
                     placeholder="输入你的EVM地址一行一个"
@@ -82,12 +119,12 @@ function App() {
                     value={input}
                 />
                 <Space style={{margin: '10px 0', justifyContent: 'flex-end'}}>
-                    <Button type="primary" onClick={check}>
+                    <Button type="primary" onClick={check} disabled={loading}>
                         查询
                     </Button>
                     <Button type="default" onClick={() => {
-                        setdata([]);
-                    }}>
+                        setData([]);
+                    }} disabled={loading}>
                         清空
                     </Button>
                 </Space>
@@ -96,7 +133,7 @@ function App() {
                 <Table dataSource={data} columns={columns} size="small" pagination={false}/>
             </Card>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
